fa447939fef5f9b55f21044b050ec6f775103efe
NisargShah <2121nisarg@gmail.com>
2023-01-30 19:59:59 +0530

    Completed

---
+++ user/includes/footer.php
@@ -0,0 +1,13 @@
+ <!-- Footer Start-->
+    <div class="footer-copyright-area">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="footer-copy-right">
+                         <p>Copyright; 2022 Digital Birth Certicate System.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Footer End-->
